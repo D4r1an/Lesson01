c05169b1a03c990885d836eb47ed889b434bf42e
Darian Marius Chirca <D4r1an@outlook.es>
2020-03-24 15:07:31 +0100

    Final for lesson01

---
+++ Scenario_1.3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario_1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Divide(18, 0.2));
+        }
+
+        public static double Divide(double x, double y)
+        {
+            var result = 0.0;
+
+            try
+            {
+                result = x / y;
+            }
+            catch (DivideByZeroException dze)
+            {
+                Console.WriteLine(dze.Message);
+            }
+            catch (InvalidCastException ice)
+            {
+                Console.WriteLine(ice.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return result;
+        }
+    }
+}
---
+++ Scenario_1.4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario_1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountDigits(10111));
+        }
+
+        public static int CountDigits(int n)
+        {
+            var digits = n.ToString().Length;
+            if (digits == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                CountDigits(digits - 1);
+                return Convert.ToInt32(digits);
+            }
+        }
+    }
+}
